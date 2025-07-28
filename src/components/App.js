ab0bd410e4a453505bd08c984aa8f79a5a7ffd50
moen <moenr49@gmail.com>
2025-07-28 23:18:51 +0300

    full file update committ

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setImageUrl(data.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      {imageUrl ? (
+        <img src={imageUrl} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
